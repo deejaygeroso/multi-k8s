90f5b815ca313caf919911cef366b5da6270f234
Deejay Geroso <deejay@adally.com>
2020-07-14 19:42:33 +0800

    Updated docker id from stephengrider to deejaygeroso

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t deejaygeroso/multi-client:latest -t deejaygeroso/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t deejaygeroso/multi-server:latest -t deejaygeroso/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t deejaygeroso/multi-worker:latest -t deejaygeroso/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push deejaygeroso/multi-client:latest
+docker push deejaygeroso/multi-server:latest
+docker push deejaygeroso/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push deejaygeroso/multi-client:$SHA
+docker push deejaygeroso/multi-server:$SHA
+docker push deejaygeroso/multi-worker:$SHA
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=deejaygeroso/multi-client:$SHA
+kubectl set image deployments/server-deployment server=deejaygeroso/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=deejaygeroso/multi-worker:$SHA
